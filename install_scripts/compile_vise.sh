b2b94d4dc12308dc5b131b5290f22c86c3c34f9b
Abhishek Dutta <thelinuxmaniac@gmail.com>
2017-03-16 11:33:50 +0000

    compilation script now exports environment vars

--- install_scripts/compile_vise.sh
+++ install_scripts/compile_vise.sh
@@ -1,21 +1,33 @@
 BUILD_DIR=$(pwd)"/../build/"
 VISE_ROOTDIR=$(pwd)"/../"
 
-if [ -d "$BUILD_DIR" ]; then
-    rm -fr $BUILD_DIR
-fi
-
-mkdir $BUILD_DIR
-
-
 FASTANN_LIBDIR=$VISE_ROOTDIR"dep/lib/fastann/"
 IMAGEMAGICK_LIBDIR=$VISE_ROOTDIR"dep/lib/imagemagick/"
 PROTOBUF_LIBDIR=$VISE_ROOTDIR"dep/lib/protobuf/"
 BOOST_LIBDIR=$VISE_ROOTDIR"dep/lib/boost/"
 VISE_CMAKE_PREFIX_PATH=$FASTANN_LIBDIR";"$IMAGEMAGICK_LIBDIR";"$PROTOBUF_LIBDIR
 
-cd $BUILD_DIR
-CC=gcc-5 CXX=g++-5 cmake -Wno-dev -DCMAKE_PREFIX_PATH=$VISE_CMAKE_PREFIX_PATH -DBOOST_ROOT=$BOOST_LIBDIR $VISE_ROOTDIR"src/"
+if [ -d "$BUILD_DIR" ]; then
+    #rm -fr $BUILD_DIR
+    echo "Build dir already exists! "$BUILD_DIR
+    exit
+else
+    mkdir $BUILD_DIR
+    cd $BUILD_DIR
+    CC=gcc-5 CXX=g++-5 cmake -Wno-dev -DCMAKE_PREFIX_PATH=$VISE_CMAKE_PREFIX_PATH -DBOOST_ROOT=$BOOST_LIBDIR $VISE_ROOTDIR"src/"
+    make -j8 api_v2
 
-make -j8 api_v2
-export DYLD_FALLBACK_LIBRARY_PATH=$FASTANN_LIBDIR"lib/:"$PROTOBUF_LIBDIR"lib/:"$BOOST_LIBDIR"lib/"
+    # export environment variables
+    if [[ ! -f ~/.bash_profile ]]; then
+        touch ~/.bash_profile
+    fi
+
+    if grep -q "export VISE_ROOTDIR" ~/.bash_profile; then
+        echo "~/.bash_profile already contains VISE environment variable!"
+    else
+        echo export DYLD_FALLBACK_LIBRARY_PATH=$FASTANN_LIBDIR"lib/:"$PROTOBUF_LIBDIR"lib/:"$BOOST_LIBDIR"lib/" >> ~/.bash_profile
+        echo export VISE_ROOTDIR=$VISE_ROOTDIR >> ~/.bash_profile
+        echo export VISE_BUILDDIR=$BUILD_DIR >> ~/.bash_profile
+        source ~/.bash_profile
+    fi
+fi
