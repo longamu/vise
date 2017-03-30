42633581bf85d2bc513fd1c8f7e6a44770cece7e
Abhishek Dutta <thelinuxmaniac@gmail.com>
2017-03-30 17:35:40 +0100

    vise_train.sh script now supports full training pipeline

--- vise_train.sh
+++ vise_train.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -u
+
 # Script to train VISE system on a custom set of images. This allows the VISE
 # system to be used to search this custom set of images.
 #
@@ -9,7 +12,7 @@
 # 20 March 2017
 #
 
-VISE_ROOTDIR="/home/tlm/dev/vise/"
+VISE_ROOTDIR=$(pwd)
 
 printf "Script to train the VGG Image Search Engine (VISE)"
 printf "\nFor more details, see www.robots.ox.ac.uk/~vgg/software/vise"
@@ -17,47 +20,86 @@ printf "\n\n"
 #
 # Ask the user to point to the location of all images
 #
-read -e -p "Enter the location of images (default: ~/data/datasets/vise/data/image/sample_training/images/) : " IMAGE_DIR
+read -e -p "Enter the location of images (default: ox5k): " IMAGE_DIR
 
-if [[ -z "$IMAGE_DIR" ]]; then
-  IMAGE_DIR="~/data/datasets/vise/data/image/sample_training/images/"
+if [[ $IMAGE_DIR != *[/]* ]]; then
+    if [[ -z "$IMAGE_DIR" ]]; then
+        #echo "You must enter the location of images"
+        #exit
+        IMAGE_DIR="~/data/datasets/vise/data/image/ox5k/oxc1"
+    else
+        IMAGE_DIR="~/data/datasets/vise/data/image/"$IMAGE_DIR
+    fi
 fi
-printf "\nIMAGE_DIR= "$IMAGE_DIR
 
 if [[ ! -d "`eval echo ${IMAGE_DIR//>}`" ]]; then
   printf "\n\tImages directory does not exist! "$IMAGE_DIR"\n"
   exit
 fi
+printf "\tVISE_ROOTDIR = "${VISE_ROOTDIR}
+printf "\n\tImage location = "${IMAGE_DIR}
+
+DATASET_NAME=$(basename ${IMAGE_DIR})
+printf "\n\tDATASET = "${DATASET_NAME}
 
 IMAGE_DIR=`eval echo ${IMAGE_DIR//>}`
-TRAIN_DATA_DIR=$IMAGE_DIR"../vise_traindata/"
-TRAIN_IMG_LIST=$TRAIN_DATA_DIR"img_list.txt"
+VISE_DATADIR="${IMAGE_DIR}/../${DATASET_NAME}_visedata/"
+
+IMLIST="${VISE_DATADIR}imlist.txt"
 
 # expand ~ to home directory before checking dir. for existence
-if [[ ! -d "`eval echo ${TRAIN_DATA_DIR//>}`" ]]; then
-  mkdir -p $TRAIN_DATA_DIR
+if [[ ! -d "${VISE_DATADIR}" ]]; then
+  mkdir -p $VISE_DATADIR
 fi
-printf "\nTRAIN_DATA_DIR= "$TRAIN_DATA_DIR
+printf "\n\tVISE_DATADIR= "$VISE_DATADIR
 
-printf "\nimlist= "$TRAIN_IMG_LIST
-(cd $IMAGE_DIR; find . -type f > $TRAIN_IMG_LIST)
+TMPDIR="${VISE_DATADIR}tmp/"
+if [[ ! -d "${TMPDIR}" ]]; then
+    mkdir $TMPDIR
+fi
 
 #
 # Build image list
 #
+printf "\n\timlist= "$IMLIST
+(cd $IMAGE_DIR; find . -type f > $IMLIST)
+
+#
+# Build configuration file
+#
+CONF="${VISE_DATADIR}conf.txt"
+echo "[${DATASET_NAME}]" > "${CONF}"
+echo "titlePrefix= ${DATASET_NAME}" >> "${CONF}"
+echo "RootSIFT= true" >> "${CONF}"
+echo "SIFTscale3= true" >> "${CONF}"
+echo "hammEmbBits= 64" >> "${CONF}"
+echo "imagelistFn= ${IMLIST}" >> "${CONF}"
+echo "databasePath= ${IMAGE_DIR}/" >> "${CONF}"
+echo "dsetFn= ${VISE_DATADIR}dset.v2bin" >> "${CONF}"
+echo "clstFn= ${VISE_DATADIR}clst.e3bin" >> "${CONF}"
+echo "iidxFn= ${VISE_DATADIR}iidx.v2bin" >> "${CONF}"
+echo "fidxFn= ${VISE_DATADIR}fidx.v2bin" >> "${CONF}"
+echo "wghtFn= ${VISE_DATADIR}wght.v2bin" >> "${CONF}"
+echo "tmpDir= ${TMPDIR}" >> "${CONF}"
+echo "trainImagelistFn= ${IMLIST}" >> "${CONF}"
+echo "trainDatabasePath= ${IMAGE_DIR}/" >> "${CONF}"
+echo "trainNumDescs= 800000" >> "${CONF}"
+echo "vocSize= 10000" >> "${CONF}"
+echo "trainFilesPrefix= ${VISE_DATADIR}train_" >> "${CONF}"
+echo "pathManHide= ${IMAGE_DIR}/" >> "${CONF}"
 
 #
 # train desc
 #
 printf "\ntrainDescs : \n"
-TRAIN_DESC=$TRAIN_DATA_DIR"train_descs.e3bin"
+TRAIN_DESC="${VISE_DATADIR}train_descs.e3bin"
 
 if [[ ! -f $TRAIN_DESC ]]; then
-  (cd $VISE_ROOTDIR"build"; 
+  (cd "${VISE_ROOTDIR}/build";
   date ;
-  ./v2/indexing/compute_index_v2 trainDescs sample_training ;
+  ./v2/indexing/compute_index_v2 trainDescs "${DATASET_NAME}" "${CONF}" ;
   date )
-else 
+else
   echo "*** Skipped trainDescs"
 fi
 
@@ -65,12 +107,12 @@ fi
 # Computer clusters
 #
 printf "\ncompute_clusters : \n"
-CLST=$TRAIN_DATA_DIR"clst.e3bin"
+CLST="${VISE_DATADIR}clst.e3bin"
 
 if [[ ! -f $CLST ]]; then
-  (cd $VISE_ROOTDIR"build" ; 
+  (cd "${VISE_ROOTDIR}/build" ;
   date ;
-  python ../src/v2/indexing/compute_clusters.py sample_training ;
+  python ../src/v2/indexing/compute_clusters.py "${DATASET_NAME}" "${CONF}" ;
   date
   )
 else
@@ -81,14 +123,14 @@ fi
 # train assign
 #
 printf "\ntrainAssign : \n"
-TRAIN_DESC=$TRAIN_DATA_DIR"train_*_ assign.e3bin"
+TRAIN_ASSIGN="${VISE_DATADIR}train_*_ assigns.bin"
 
-if [[ ! -f $TRAIN_DESC ]]; then
-  (cd $VISE_ROOTDIR"build"; 
+if [[ ! -f $TRAIN_ASSIGN ]]; then
+  (cd "${VISE_ROOTDIR}/build";
   date ;
-  ./v2/indexing/compute_index_v2 trainAssign sample_training ;
+  ./v2/indexing/compute_index_v2 trainAssign "${DATASET_NAME}" "${CONF}" ;
   date )
-else 
+else
   echo "*** Skipped trainAssign"
 fi
 
@@ -97,14 +139,14 @@ fi
 # train hamm
 #
 printf "\ntrainHamm : \n"
-TRAIN_DESC=$TRAIN_DATA_DIR"train_hamm.e3bin"
+TRAIN_HAMM="${VISE_DATADIR}train_*_hamm64.v2bin"
 
-if [[ ! -f $TRAIN_DESC ]]; then
-  (cd $VISE_ROOTDIR"build"; 
+if [[ ! -f $TRAIN_HAMM ]]; then
+  (cd "${VISE_ROOTDIR}/build";
   date ;
-  ./v2/indexing/compute_index_v2 trainHamm sample_training ;
+  ./v2/indexing/compute_index_v2 trainHamm "${DATASET_NAME}" "${CONF}" ;
   date )
-else 
+else
   echo "*** Skipped trainHamm"
 fi
 
@@ -112,14 +154,14 @@ fi
 # perform indexing
 #
 printf "\nindex : \n"
-TRAIN_DESC=$TRAIN_DATA_DIR"train_index.e3bin"
+TRAIN_INDEX="${VISE_DATADIR}dset.v2bin"
 
-if [[ ! -f $TRAIN_DESC ]]; then
-  (cd $VISE_ROOTDIR"build"; 
+if [[ ! -f $TRAIN_INDEX ]]; then
+  (cd "${VISE_ROOTDIR}/build";
   date ;
-  ./v2/indexing/compute_index_v2 index sample_training ;
+  ./v2/indexing/compute_index_v2 index "${DATASET_NAME}" "${CONF}" ;
   date )
-else 
+else
   echo "*** Skipped index"
 fi
 printf "\nDone\n"
