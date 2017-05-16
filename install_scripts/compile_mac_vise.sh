261dcfdd680638937f0776bd04dc0ed813238caf
Abhishek Dutta <thelinuxmaniac@gmail.com>
2017-05-16 10:26:39 +0100

    compile_mac_vise.sh script now compiles pypar and dkmeans_relja

--- install_scripts/compile_mac_vise.sh
+++ install_scripts/compile_mac_vise.sh
@@ -5,7 +5,34 @@ FASTANN_LIBDIR=$VISE_ROOTDIR"../vise_dep/lib/fastann/"
 IMAGEMAGICK_LIBDIR=$VISE_ROOTDIR"../vise_dep/lib/imagemagick/"
 PROTOBUF_LIBDIR=$VISE_ROOTDIR"../vise_dep/lib/protobuf/"
 BOOST_LIBDIR=$VISE_ROOTDIR"../vise_dep/lib/boost/"
+LOCAL_LIB_DIR="/usr/local/lib"
 VISE_CMAKE_PREFIX_PATH=$FASTANN_LIBDIR";"$IMAGEMAGICK_LIBDIR";"$PROTOBUF_LIBDIR
+
+# export environment variables
+if [[ ! -f ~/.bash_profile ]]; then
+    touch ~/.bash_profile
+fi
+
+if grep -q "export VISE_ROOTDIR" ~/.bash_profile; then
+    echo "~/.bash_profile already contains VISE environment variable!"
+else
+    echo export DYLD_FALLBACK_LIBRARY_PATH=$FASTANN_LIBDIR"lib/:"$PROTOBUF_LIBDIR"lib/:"$BOOST_LIBDIR"lib/:"$LOCAL_LIB_DIR >> ~/.bash_profile
+    echo export VISE_ROOTDIR=$VISE_ROOTDIR >> ~/.bash_profile
+    echo export VISE_BUILDDIR=$BUILD_DIR >> ~/.bash_profile
+    source ~/.bash_profile
+fi
+
+#
+# Compile pypar and dkmeans_relja
+#
+( cd $VISE_ROOTDIR"src/external/dkmeans_relja/pypar_2.1.4_94/source" ;
+  python setup.py build ;
+  sudo python setup.py install
+)
+( cd $VISE_ROOTDIR"src/external/dkmeans_relja/" ;
+  python setup.py build ;
+  sudo python setup.py install ;
+)
 
 if [ -d "$BUILD_DIR" ]; then
     #rm -fr $BUILD_DIR
@@ -16,18 +43,4 @@ else
     cd $BUILD_DIR
     CC=gcc-6 CXX=g++-6 cmake -Wno-dev -DCMAKE_PREFIX_PATH=$VISE_CMAKE_PREFIX_PATH -DBOOST_ROOT=$BOOST_LIBDIR $VISE_ROOTDIR"src/"
     make -j8 all
-
-    # export environment variables
-    if [[ ! -f ~/.bash_profile ]]; then
-        touch ~/.bash_profile
-    fi
-
-    if grep -q "export VISE_ROOTDIR" ~/.bash_profile; then
-        echo "~/.bash_profile already contains VISE environment variable!"
-    else
-        echo export DYLD_FALLBACK_LIBRARY_PATH=$FASTANN_LIBDIR"lib/:"$PROTOBUF_LIBDIR"lib/:"$BOOST_LIBDIR"lib/" >> ~/.bash_profile
-        echo export VISE_ROOTDIR=$VISE_ROOTDIR >> ~/.bash_profile
-        echo export VISE_BUILDDIR=$BUILD_DIR >> ~/.bash_profile
-        source ~/.bash_profile
-    fi
 fi
