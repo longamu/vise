1459b74cdd62aa4eb6781b428685b9984cd195c3
Abhishek Dutta <thelinuxmaniac@gmail.com>
2018-07-02 09:45:07 +0100

    added scripts to run ox5k demo; fixed dynamic_image with "H is not None"

---
+++ scripts/0x04_start_ox5k_backend.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /home/tlm/dev/vise/bin
+LD_LIBRARY_PATH="/home/tlm/deps/vise/lib/lib" ./api_v2 10001 ox5k /home/tlm/mydata/vise/repo/ox5k/1/vise_data/config.txt
+
---
+++ scripts/0x04_start_ox5k_frontend.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /home/tlm/dev/vise/src/search_engine/relja_retrival/src/ui/web
+python webserver.py 10002 ox5k 10001 1 /home/tlm/mydata/vise/repo/ox5k/1/vise_data/config.txt
