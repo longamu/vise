c9ebc9a1b3338f525a25a56b1efadd6776d344eb
Abhishek Dutta <thelinuxmaniac@gmail.com>
2018-08-14 10:53:16 +0100

    removed fastann cmake files

--- src/external/fastann/cmake_build/CMakeFiles/perf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/perf"
-  "dummy_perf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/perf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
