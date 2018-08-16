2eefebd67cf3aecf72c1c3cf4501e29b83051211
Abhishek Dutta <thelinuxmaniac@gmail.com>
2018-08-16 09:30:02 +0100

    updated

--- src/ui/web/home.py
+++ src/ui/web/home.py
@@ -18,12 +18,12 @@ class home:
   <div class="top_description">15cILLUSTRATION is a searchable database of 15th-century printed illustrations developed by the <a href="http://15cbooktrade.ox.ac.uk/">15cBOOKTRADE</a> project in collaboration with the <a href="http://www.robots.ox.ac.uk/~vgg/">Visual Geometry Group</a>. 15cILLUSTRATION is the first comprehensive and systematic tool to track and investigate the production, use, circulation, and copy of woodblocks, iconographic subjects, artistic styles, etc. within 15th-century printed illustrated editions.</div>
 
   <div class="task pagerow">
-    <a href="page0">
     <div class="task_i task1_bg_color">
+    <a href="page0">
       <div class="task_title">Search Using Database Images</div>
       <div class="task_description">This searchable database currently contains %d printed illustrations from the 15th century which can be searched using one of the images already in this database.</div>
-    </div>
     </a>
+    </div>
 
     <a href="text_search">
     <div class="task_i task2_bg_color">
@@ -34,7 +34,7 @@ class home:
 
     <div class="task_i task3_bg_color">
       <div class="task_title">Upload and Search</div>
-      <div class="task_description">The 15th-century printed illustrations can also be searched using a new image uploaded from your computer.</div>
+      <div class="task_description">The 15th-century printed illustrations can also be searched using a new image uploaded from your computer. In the top right corner of this page, clcik "Browse" button to upload an image and click "Upload and Search" to search using this image.</div>
     </div>
 
     <div style="clear:both"></div>
